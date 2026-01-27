91c113cf49550fd7e54451ecc78e980cbc61706f
João Gallo <joaogallohd@gmail.com>
2026-01-27 16:21:08 -0300

    feat: implementad the global validation for the requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    //load environment variable from .env
+    ConfigModule.forRoot({
+      isGlobal: true, //make ConfigService avaliable erverywhere without  re-importing
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //Global Validation for all incoming requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //strips properites that do not have decorates
+      forbidNonWhitelisted: true, //throw an error if non-whiteList properites are presents
+      transform: true, //transform payload to DTO instance and converts types
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
 }
+
 bootstrap();
